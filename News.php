a3a75ed655cc5494555023d26622685024627485
Paul59380 <paul.genly59380@gmail.com>
2019-03-15 17:09:14 +0100

    Final code for method getListNews in NewsManager Final code for method "hydrate" in News

---
+++ News.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paulg
+ * Date: 15/03/2019
+ * Time: 14:51
+ */
+
+class News
+{
+    protected $id;
+    protected $id_author;
+    protected $title_news;
+    protected $contains_news;
+    protected $date_create;
+
+    public function __construct($data)
+    {
+        $this->hydrate($data);
+    }
+
+    public function hydrate($data)
+    {
+        foreach ($data as $key => $value) {
+            $method = 'set' . ucfirst($key);
+                $this->$method($value);
+        }
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id_author
+     */
+    public function setId_author($id_author)
+    {
+        $this->id_author = $id_author;
+    }
+
+    public function getIdAuthor()
+    {
+        return $this->id_author;
+    }
+
+    /**
+     * @param mixed $title_news
+     */
+    public function setTitle_news($title_news)
+    {
+        $this->title_news = $title_news;
+    }
+
+    public function getTitleNews()
+    {
+        return $this->title_news;
+    }
+
+    /**
+     * @param mixed $contains_news
+     */
+    public function setContains_news($contains_news)
+    {
+        $this->contains_news = $contains_news;
+    }
+
+    public function getContainsNews()
+    {
+        return $this->contains_news;
+    }
+
+    /**
+     * @param mixed $date_create
+     */
+    public function setDate_create($date_create)
+    {
+        $this->date_create = $date_create;
+    }
+
+    public function getDateCreate()
+    {
+        return $this->date_create;
+    }
+}
---
+++ NewsManager.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paulg
+ * Date: 15/03/2019
+ * Time: 14:55
+ */
+
+class NewsManager
+{
+    protected $db;
+
+    /**
+     * @param mixed $db
+     */
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    /**
+     * @return mixed|PDO
+     */
+    public function getDb()
+    {
+        return $this->db;
+    }
+
+    public function setDb(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function getNews($infoNews)
+    {
+
+    }
+
+    public function getListNews()
+    {
+        $news = [];
+        $q = $this->db->query('SELECT * FROM news');
+        while($data = $q ->fetch(PDO::FETCH_ASSOC))
+        {
+            $news[] = new News($data);
+        }
+        return $news;
+    }
+
+    public function getCommentsNews($infosNews, $infosComment)
+    {
+
+    }
+}

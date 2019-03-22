099ca471b5653be1aa6ef55d3bf13612830d7a45
Paul59380 <paul.genly59380@gmail.com>
2019-03-22 11:11:25 +0100

    push navConnect and navPublic

---
+++ view/frontend/navigationConnect.php
@@ -0,0 +1,26 @@
+<nav>
+    <ul class="allNavigationBar">
+        <?php
+        if (isset($_SESSION['name'])){
+            echo "<li id='userSession'>" .$_SESSION['name'] . "</li>";
+        };
+        ?>
+        <li class="logoMontain"><a href="index.php"> <i class="fas fa-mountain fa-2x"></i> </a> </li>
+        <li><a href="index.php">Acceuil</a></li>
+        <li><a href="decouvrez_moi">Découvrez moi</a></li>
+        <?php
+        if (!isset($_SESSION['name'])){
+            echo "<li><a href=\"connexion.php\">Se connecter</a></li>";
+        };
+        ?>
+        <li><a href="?deconnexion=1">Déconnexion</a> </li>
+    </ul>
+</nav>
+
+<?php
+if(isset($_GET['deconnexion']))
+{
+    header('Location:index.php');
+    session_destroy();
+}
+?>
---
+++ view/frontend/navigationPublic.php
@@ -0,0 +1,9 @@
+
+<nav>
+    <ul class="allNavigationBar">
+        <li class="logoMontain"><a href="index.php"> <i class="fas fa-mountain fa-2x"></i> </a> </li>
+        <li><a href="index.php">Acceuil</a></li>
+        <li><a href="decouvrez_moi">Découvrez moi</a></li>
+        <li><a href=connexion.php>Se connecter</a></li>
+    </ul>
+</nav>

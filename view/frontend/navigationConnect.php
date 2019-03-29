3964a61f8fd132ffb2b7c1e2416acfb0026d0769
Paul59380 <paul.genly59380@gmail.com>
2019-03-29 16:33:29 +0100

    Change design in nav

--- view/frontend/navigationConnect.php
+++ view/frontend/navigationConnect.php
@@ -4,10 +4,8 @@
         if (isset($_SESSION['name'])){
             echo "<li id='userSession'><a style='color: #01991f' href='index.php'> " .$_SESSION['name'] . "</a></li>";
         };
-        ?>
-        <li class="logoMontain"><a href="index.php"> <i class="fas fa-igloo fa-2x"></i> </a> </li>
-        <li><a href="index.php">Acceuil</a></li>
-        <li><a href="decouvrez_moi">Découvrez moi</a></li>
+        ?><li class="logoMontain"><a href="index.php"> <i class="fas fa-igloo fa-2x"></i> </a> </li>
+        <li><a style="margin-left: 20%" href="index.php">Acceuil</a></li>
         <?php
         if (!isset($_SESSION['name'])){
             echo "<li><a href=\"connexion.php\">Se connecter</a></li>";
--- view/frontend/navigationPublic.php
+++ view/frontend/navigationPublic.php
@@ -2,8 +2,7 @@
 <nav>
     <ul class="allNavigationBar">
         <li class="logoMontain"><a href="index.php"> <i id="montain" class="fas fa-igloo fa-2x"></i></i> </a> </li>
-        <li><a href="index.php">Acceuil</a></li>
-        <li><a href="decouvrez_moi">Découvrez moi</a></li>
+        <li><a style="margin-left: 30%" href="index.php">Acceuil</a></li>
         <li><a href=connexion.php><i class="fas fa-user-circle fa-2x"></i></a></li>
     </ul>
 </nav>
